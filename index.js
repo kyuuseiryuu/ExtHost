73fc6f97a0ea82b3e956f363cb6d666ef1260bd7
seiryuukyuu <kyuuseiryuu@gmail.com>
2017-09-14 10:10:42 +0800

    添加初始化命令 init 创建基础配置

--- index.js
+++ index.js
@@ -7,11 +7,13 @@ const argv = require('yargs')
     .command('l', '(list) 列出所有的 host 配置', commander.list)
     .command('c', '(choose) 选择一个 host 配置进行操作', commander.choose)
     .command('u', '(use) 启用一个或者多个 host 配置', commander.use)
+    .command('init', '初始化一些通用的默认配置', commander.init)
     .usage('Usage: eh [options]')
     .help('h')
     .epilog('By: KyuuSeiryuu 2017/09/14')
     .argv;
 
-console.log('请注意：请先给你的 host 文件赋予当前用户的可写权限。');
+console.log('------------------------------------------------------------------------------');
+console.log('友情提示：请先给你的 host 文件赋予当前用户的可写权限。避免 hosts 文件无法写入的尴尬情况。');
 console.log('host 文件路径：/etc/hosts (*unix)');
 console.log('输入：eh -h 获取帮助');
--- readme.md
+++ readme.md
@@ -2,8 +2,22 @@
 
 一款在配置 hosts 文件的命令行工具 （暂只支持 *UNIX 平台)
 
+## 理念
+
+所有的配置都可以被管理起来，完全接管系统 host 配置。
+
 ## Quick Start
 
 `npm i exthost`
 
 `eh -h`
+
+## How to use
+
+` eh a test -I=127.0.0.1 -H=localhost,www.foo.com,foo.com `  
+
+> 创建一个名为 test 的 host 配置，并初始化一些信息
+
+` eh c test -I=192.168.0.1 -H=gateway`
+
+> test 配置文件中添加记录 `gateway` 指向 `192.168.0.1`
