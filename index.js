abdb18043c35490d9803f6320a8a10f6dc60e692
seiryuukyuu <kyuuseiryuu@gmail.com>
2017-09-14 09:10:45 +0800

    修改命令描述

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 const commander = require('./lib/commanders');
 const argv = require('yargs')
-    .command('a', '(add) 创建一个空的 host 配置，更多额外参数可添加 -h 参数查看', commander.create)
+    .command('a', '(add) 创建一个空的 host 配置并追踪，更多额外参数可添加 -h 参数查看', commander.create)
     .command('d', '(delete) 删除一个 host 配置', commander.remove)
     .command('s', '(show) 显示一个 host 的详细配置', commander.show)
     .command('l', '(list) 列出所有的 host 配置', commander.list)
